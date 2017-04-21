9a109aea3426a9b7acc5884feffef3d08d813be4
william <740056233@qq.com>
2017-04-21 12:14:07 +0800

    增加提取hostname函数

--- src/common/js/string.js
+++ src/common/js/string.js
@@ -4,7 +4,7 @@ export function chop(length) {
   return function (str) {
     let nstr = '';
     let maxLength = length + Math.floor(length / 5);
-    if( str === undefined || str.length < maxLength) {
+    if (str === undefined || str.length < maxLength) {
       return str;
     }
     for (let i = 0; i < maxLength; i += 1) {
@@ -12,4 +12,26 @@ export function chop(length) {
     }
     return `${nstr}...`;
   };
+}
+
+export function htmlToText(html) {
+  let text = "";
+  let ignore = false;
+  for (let i = 0; i < html.length; i++) {
+    let c = html.charAt(i);
+    if (ignore) {
+      if (c === '<') {
+        // 开启忽略模式
+        ignore = true;
+      } else {
+        text += c;
+      }
+    } else {
+      if (c === '>') {
+        // 开始接收字符串
+        ignore = false;
+      }
+    }
+  }
+  return text;
 }
