8e895911c4525662dd13fc1b1520003110f476a7
william <740056233@qq.com>
2017-04-21 11:13:04 +0800

    增加MainPins的border-bottom，增加data中的mainPins的数据

--- src/components/userpage/MainPins.vue
+++ src/components/userpage/MainPins.vue
@@ -1,38 +1,46 @@
 <template>
-  <div class="pins-module">
+  <div class="main-pins-module">
     <div class="header">
       <router-link to="/self/pins/"
-                   class="link" exact>我的分享</router-link>
+                   class="link"
+                   exact>我的分享</router-link>
       <router-link to="/self/pins/posts"
                    class="link">我的文章</router-link>
       <router-link to="/self/pins/columns"
                    class="link">我的专栏</router-link>
     </div>
-    <router-view>
-
+    <router-view :user="user" :detail="detail">
     </router-view>
   </div>
 </template>
 
 <script type="text/javascript">
 export default {
-
-
+  props: {
+    user: {
+      type: Object,
+    },
+    detail: {
+      type: Object,
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import '../../common/scss/mixin';
-  .pins-module {
-    padding: 0 16px;
-    .header {
-      .link {
-        padding: 6px 0 7px 0;
-        margin-right: 36px;
-      }
-      .router-link-active {
-        @include bold-title(50px);
-      }
+
+.main-pins-module {
+  padding: 0 16px;
+  .header {
+    @include border-bottom;
+    .link {
+      padding: 6px 0 7px 0;
+      margin-right: 36px;
+    }
+    .router-link-active {
+      @include bold-title(50px);
     }
   }
+}
 </style>
