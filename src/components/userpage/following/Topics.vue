e84c3153b458faf7a8d1981a1cac83c704fbfdbb
william <740056233@qq.com>
2017-04-23 00:29:51 +0800

    完成话题组件

--- src/components/userpage/following/Topics.vue
+++ src/components/userpage/following/Topics.vue
@@ -1,16 +1,46 @@
 <template>
-  <div>
-    你关注的话题
+  <div class="topics-module">
+    <div v-if="detail.followingPart"
+         class="topics">
+      <div v-for="topic in detail.followingPart.topics"
+           class="topic list-item">
+        <img alt="话题图片"
+             class="avatar"
+             :src="topic.avatar">
+        <div class="content">
+          <div class="bold-title">{{topic.title}}</div>
+          <span class="answer-num">{{topic.answerNum}} 个回答</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/javascript">
 export default {
-
-
+  props: {
+    detail: {
+      type: Object,
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
+@import '../../../common/scss/userpage';
+@import '../../../common/scss/mixin';
+.topic {
+  display: flex;
+  @include border-bottom;
+  .content {
+    &:hover {
+      color: inherit;
+    }
+  }
+  .answer-num {
+    font-size: 14px;
+    color: #175199;
+    cursor: initial;
+  }
+}
 </style>
